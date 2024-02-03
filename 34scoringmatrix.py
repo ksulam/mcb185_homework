9f64436a7ede210ec6948d0016dc9ddd5ad6eaf0
ksulam <ksulam@ucdavis.edu>
2024-02-02 17:29:47 -0800

    new

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -1,16 +1,17 @@
 
 nts = 'ACGT'
 
-print(" ", end='')
-for nt in nts:
-	print(" ", nt, end='')
-print()
+print(" ", end='') # print first space in 1st row
+
+for nt in nts: 
+	print(" ", nt, end='') # print 1st row
+
+print() 
 	
 for nt1 in nts:
-	print(nt1, end='')
+	print(nt1, end='') # print nt in column
 	
 	for nt2 in nts:
-	
 		if nt1 == nt2: print(" +1", end='')
 		else: print(" -1", end='')
-	print()
+	print() 
