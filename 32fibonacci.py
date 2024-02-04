a7fdb9c67dbfb0149ddd788d2481675dd809e78e
ksulam <ksulam@ucdavis.edu>
2024-02-04 11:12:50 -0800

    fixed code issues

--- 32fibonacci.py
+++ 32fibonacci.py
@@ -1,10 +1,11 @@
 def fibonacci(n):
 	n1 = -1
 	n2 = 1
+	
 	for i in range(n):
 		total = n1 + n2
-		n1 = n2
-		n2 = total 
+		n1 = n2 #n1 takes on n2's prev. value
+		n2 = total # n2 takes on value of n1+n2
 		print(total)
 
 fibonacci(10)
