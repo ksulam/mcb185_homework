e862115b13b6ac6e7bbfa54a66b2ca263740f0b1
ksulam <ksulam@ucdavis.edu>
2024-02-03 19:25:28 -0800

    new

--- 33triples.py
+++ 33triples.py
@@ -4,9 +4,6 @@ def is_perfect_square(n):
 	if root % 1 == 0: return True
 	return False
 
-def isinteger(n):
-	if n == n // 1: return True
-	return False
 count = 0
     	
 for a in range(1, 101): 
@@ -16,4 +13,3 @@ for a in range(1, 101):
 			print(a, b, int(math.sqrt(csquared)))
 			count = count + 1
 				
-print("total perfect triples: ", count)
