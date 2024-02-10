0cfbcc94694adc149983a4ef6f4e2310143b8a29
ksulam <ksulam@ucdavis.edu>
2024-02-09 19:21:35 -0800

    edits

--- 43randomdna.py
+++ 43randomdna.py
@@ -1,13 +1,11 @@
 import random 
-i = 0
 
-def dna(sequence):
-	for i in range(sequence):
-		i += 1
-		print(f'\n>seq-{i}')
-		nt = random.randint(30, 60)
 
-		for j in range(nt):
-			print(random.choice('ACGT'), end='')
+for i in range(4):
+	i += 1
+	print(f'\n>seq-{i}')
+	nt = random.randint(30, 60)
 
-dna(4)
+	for j in range(nt):
+		print(random.choice('ACGT'), end='')
+
