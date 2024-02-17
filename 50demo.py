404725e518a0397dee58facf5c49a9901c224c73
ksulam <ksulam@ucdavis.edu>
2024-02-17 02:33:52 -0800

    new

---
+++ 50demo.py
@@ -0,0 +1,13 @@
+"""
+seq = 'GAATTC'
+print(seq[0], seq[1])
+
+for i in range(len(seq)):
+	print(i, seq[i])
+"""
+	
+s = 'ABCDEFGHIJ'
+print(s[0:5])
+print(s, s[::], s[::1], s[::-1])
+
+
--- 53genomestats.py
+++ 53genomestats.py
@@ -2,23 +2,20 @@ import gzip
 import sys
 import math
 #  ~/Code/MCB185/data/A.thaliana.gff.gz 
-# Chr1	TAIR10	gene	3631	5899	.	+	.	ID=AT1G01010;Note=protein_coding_gene;Name=AT1G01010
 gffpath = sys.argv[1]
 feature = sys.argv[2]
-#fp = gzip.open("A.thaliana.gff.gz", "rt")
 
 countlengths = 0
 lengths = []
 
 with gzip.open(gffpath, 'rt') as fp:
-    for line in fp:
-    	words = line.split()
-    	#print(line)
-    	if words[2] == feature: 
-    		countlengths += 1 #count
-    		beg = int(words[3])
-    		end = int(words[4])
-    		lengths.append(end - beg + 1)  
+	for line in fp:
+		words = line.split()
+		if words[2] == feature: 
+			countlengths += 1 #count
+			beg = int(words[3])
+			end = int(words[4])
+			lengths.append(end - beg + 1)  
 
 # count 
 print("Count:", countlengths)
@@ -34,15 +31,13 @@ def minmax(vals):
 	
 mini, maxi = minmax(lengths)  
 print("Minimum:", mini)	
-print("Maximum:", maxi)
-	   
+print("Maximum:", maxi)   
    
 # mean   
 sumlengths = 0
 for val in lengths: 
-	sumlengths += val		  	
+	sumlengths += val
 mean = sumlengths / len(lengths)
-
 print("Mean:", mean)		
 
 
@@ -60,8 +55,8 @@ print("Standard Deviation:", stdev)
 # median 
 for val in lengths:
 	lengths.sort()
-	if len(lengths) % 2 == 0: # if length is even, take average of 2 middle numbers
-		x1 = lengths[len(lengths) // 2 -1]
+	if len(lengths) % 2 == 0: 
+		x1 = lengths[len(lengths) // 2 - 1]
 		x2 = lengths[len(lengths) // 2]
 		med = (x1 + x2) / 2
 	else: 
