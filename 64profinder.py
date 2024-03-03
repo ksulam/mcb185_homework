aff14dee34e8edb391bc359ceaae5f380effb07d
ksulam <ksulam@ucdavis.edu>
2024-03-03 10:35:10 -0800

    new

--- 64profinder.py
+++ 64profinder.py
@@ -2,53 +2,54 @@ import sys
 import gzip
 import mcb185
 import dogma
-
 seq = sys.argv[1]
 mini = int(sys.argv[2])
 w = 3
 
 
-
-def rollingtranslation(seq):
+def translation(seq, f):
 	aminoacids = []
-	for s in range(3):
-		for i in range(s, len(seq), 3):
-			aminoacids.append(dogma.translate(seq[i:i+3]))
+	
+	for i in range(f, len(seq), 3):
+		aminoacids.append(dogma.translate(seq[i:i+3]))
 		
 	return ''.join(aminoacids)
+
 
 # picking proteins that begin with M and are >= 100 length		
 def profinder(seq, mini):
 	proteins = []
 	aalist = seq.split('*') #split amino acids, removing last one
 	aalist.pop()
-	for aas in aalist:#splitting proteins based on stop codon 
+	for aas in aalist: 
 		start = aas.find('M')
 		if aas and len(aas[start:]) >= mini and start >= 0: #length from M is >= 100
 			proteins.append(aas[start:])
 			
 	return proteins 
 
+
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
-	translatedstrand1 = rollingtranslation(seq) 
-	translatedstrand2 = rollingtranslation(dogma.revcomp(seq)) 
+	translatedstrands = []
+	
+	for s in range(0, 3):
+		translatedstrands.append(translation(seq, s))
+		translatedstrands.append(translation(dogma.revcomp(seq), s))
 	
 	#selecting proteins from each strand
-	finalproteins1 = profinder(translatedstrand1, mini) 
-	finalproteins2 = profinder(translatedstrand2, mini)
+	finalproteins = []
+	for strand in translatedstrands:
+		finalproteins.append(profinder(strand, mini)) 
 
-	#combining proteins from each strand to one
-	for protein in finalproteins2:	
-		finalproteins1.append(protein)
 	
 	proteincount = 0	
-	for protein in finalproteins1:
-		print(f'>{defline[0:11]}-prot-{proteincount}')
-		print(protein)
-		#print(len(protein))
-		proteincount +=1
-	
-	
-	
+	for protein in finalproteins:
+		for singleprotein in protein: 
+			print(f'>{defline[0:11]}-prot-{proteincount}')
+			print(singleprotein)
+			proteincount +=1
+		
+		
+		
 
 	
