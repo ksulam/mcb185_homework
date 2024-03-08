1017cc47f5988b7558285c438a9f93b9d1636f54
ksulam <ksulam@ucdavis.edu>
2024-03-08 14:34:07 -0800

    new

--- 74genefinder.py
+++ 74genefinder.py
@@ -8,25 +8,26 @@ minlength = int(sys.argv[2])
 
 def genefinder(s, minl):
 	geneloc = []
-	#for strand in [seq, dogma.revcomp(seq)]:
+
 	for frame in range(3):
 		frameseq = s[frame:]
 		i = frame
-	#	print(len(frameseq))
-		while i < len(frameseq):
-		
+
+		while i < len(frameseq):	
 			startcodon = frameseq[i:i+3]
+			
 			if startcodon == 'ATG':
 				start = i #first letter of start codon	
 				
 				for j in range(i, len(frameseq) -2, 3):
 					stopcodon = frameseq[j:j+3]
+					
 					if stopcodon == 'TAA' or stopcodon == 'TAG' or stopcodon == 'TGA':
 						stop = j 
-						#print(stopcodon, stop)
+					
 						if stop - start >= minl:
-							geneloc.append((start, stop))
-							
+							geneloc.append((start, stop))		
+						
 						i = stop
 						break
 			i += 3 
@@ -46,11 +47,6 @@ for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	for nt in geneloc_rev:
 		print("-", nt[0], nt[1], "+")
 
-	#print(geneloc_forward)
-	#print(geneloc_rev)
-	#print(len(geneloc_forward) + len(geneloc_rev))
-
-
 """
 geneloc_forward = genefinder(mainseq, minlength)
 for nt in geneloc_forward:
@@ -62,5 +58,5 @@ for nt in geneloc_rev:
 
 print(len(geneloc_forward))
 """
-	# turn into function so that you do it on revcomp sequence 
+
 	
