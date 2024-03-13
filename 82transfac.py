9ffb841d7eee6e3a9dabff5e36335a0f7a305a04
ksulam <ksulam@ucdavis.edu>
2024-03-12 22:22:33 -0700

    new

--- 82transfac.py
+++ 82transfac.py
@@ -4,43 +4,41 @@ import gzip
 import sys
 import re
 
-fulldict = []
+catalog = []
 
 with gzip.open(sys.argv[1], 'rt') as fp: #opening jaspar file
-	singledict = {} #creating new dict for each ID
+	singlerecord = {} #creating new record for a single ID
 	
 	for line in fp:  
 		lines = line.rstrip()
 		
-		if line.startswith('XX'):
-			continue
-			
 		if lines.startswith('ID'):
-			singledict['id']= lines.split()[1] #ID name
+			singlerecord['id']= lines.split()[1] #ID name
 			
 			
 		elif lines.startswith('PO'): #PWM data begins at this line
-			singledict['pwm'] = [] 
-			#print(singledict)
+			singlerecord['pwm'] = [] #empty pwm
+			print(singlerecord)
+	
 			
 		#line starting with # = contains row of PWM values	
-		elif lines.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+		elif lines.startswith(('0')):
 			splitpwm = line.split() #split pwm values in each row
 			#print(splitpwm)
 			
-			#adding PWM values to singledict
-			singledict['pwm'].append({'A': float(splitpwm[1]),
+			#adding PWM values to singlerecord
+			singlerecord['pwm'].append({'A': float(splitpwm[1]),
 								'C': float(splitpwm[2]),
 								'G': float(splitpwm[3]),
 								'T': float(splitpwm[4])})
 			
-		#append data to fulldict + restarting dict for newID
+		#append data to catalog + restarting record for newID
 		elif line.startswith('//'):
-			fulldict.append(singledict)
-			singledict = {}
+			catalog.append(singlerecord)
+			singlerecord = {}
 
 #json format
-jsonoutput = json.dumps(fulldict, indent=2)
-#print(jsonoutput)
+jsoncatalog = json.dumps(catalog, indent=2)
+#print(jsoncatalog)
 	
 
